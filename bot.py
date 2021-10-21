c64aa90e23440d24e0e3b14967ff2d2df06d5ac6
fab1aq <st.fab1aq@gmail.com>
2021-10-21 21:28:48 +0900

    「/wiki (単語)」 を送信し、要約を discord に送信する

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import os
 import re
 import discord
+import wikipedia
 
 exec(open("env.py").read())
 WIKI_SUMMARY_TOKEN = os.environ["WIKI_SUMMARY_TOKEN"]
@@ -20,9 +21,20 @@ async def on_message(message):
         return
     # 「/wiki」と発言したら wiki と返す処理
     if re.search('/wiki', message.content):
-        word = message.content.strip('/wiki ')
-        await message.channel.send(word)
+        search_word = message.content.strip('/wiki ')
+        print(search_word)
+        wiki_summary = WikiApi.wiki_summary(search_word)
+        await message.channel.send(wiki_summary)
 
 
 # Botの起動とDiscordサーバーへの接続
 client.run(WIKI_SUMMARY_TOKEN)
+
+class WikiApi:
+    wikipedia.set_lang("ja")
+    def wiki_summary(search_word):
+        try:
+            result = wikipedia.page(search_word)
+            return result.summary
+        except wikipedia.exceptions.DisambiguationError  as e:
+            return 'wikiの結果が上手く返ってきませんでした。より具体的なワードで検索してみてください。'

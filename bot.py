b2ad8f947936927e70bb9dbc5490f5c732636706
fab1aq <st.fab1aq@gmail.com>
2021-10-23 23:07:22 +0900

    from discord.ext import commands を利用するように

--- bot.py
+++ bot.py
@@ -1,36 +1,28 @@
 import os
 import wikipedia
-import discord
+from discord.ext import commands
 
 wikipedia.set_lang('ja')
 
 exec(open('env.py').read())
 WIKI_SUMMARY_TOKEN = os.environ['WIKI_SUMMARY_TOKEN']
 
-client = discord.Client()
+bot = commands.Bot(command_prefix='/')
 
-# 起動時に動作する処理
-@client.event
+@bot.event
 async def on_ready():
     # 起動したらターミナルにログイン通知が表示される
-    print('bot が起動しました')
+    print('ログインしました')
 
-# メッセージ受信時に動作する処理
-@client.event
-async def on_message(message):
-    # メッセージ送信者がBotだった場合は無視する
-    if message.author.bot:
-        return
-    # 「/wiki」と発言したら wiki の要約を返す
-    if message.content.startswith('/wiki'):
-        search_word = message.content.strip('/wiki ')
-        try:
-            result = wikipedia.summary(search_word)
-        except wikipedia.exceptions.DisambiguationError  as e:
-            result = '[error] 曖昧な単語です。より具体的な単語で検索してみてください。'
-        except wikipedia.exceptions.PageError  as e:
-            result = '[error] ページが存在しない単語です。別の単語で検索してみてください。'
-        await message.channel.send(result)
+@bot.command()
+async def wiki(context, search_word):
+    try:
+        result = wikipedia.summary(search_word)
+    except wikipedia.exceptions.DisambiguationError  as e:
+        result = '[error] 曖昧な単語です。より具体的な単語で検索してみてください。'
+    except wikipedia.exceptions.PageError  as e:
+        result = '[error] ページが存在しない単語です。別の単語で検索してみてください。'
+    await context.send(result)
 
 # Botの起動とDiscordサーバーへの接続
-client.run(WIKI_SUMMARY_TOKEN)
+bot.run(WIKI_SUMMARY_TOKEN)

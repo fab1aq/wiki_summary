bc82b0f4d84f614028ea1841048daa48c44a2e87
fab1aq <st.fab1aq@gmail.com>
2021-10-23 21:34:39 +0900

    細かい個所を修正

--- bot.py
+++ bot.py
@@ -1,10 +1,18 @@
 import os
-import re
-import discord
 import wikipedia
+import discord
 
-exec(open("env.py").read())
-WIKI_SUMMARY_TOKEN = os.environ["WIKI_SUMMARY_TOKEN"]
+wikipedia.set_lang('ja')
+
+def get_wiki_summary(search_word):
+    try:
+        result = wikipedia.page(search_word)
+        return result.summary
+    except wikipedia.exceptions.DisambiguationError  as e:
+        return 'wikiの結果が上手く返ってきませんでした。より具体的なワードで検索してみてください。'
+
+exec(open('env.py').read())
+WIKI_SUMMARY_TOKEN = os.environ['WIKI_SUMMARY_TOKEN']
 
 client = discord.Client()
 # 起動時に動作する処理
@@ -19,22 +27,12 @@ async def on_message(message):
     # メッセージ送信者がBotだった場合は無視する
     if message.author.bot:
         return
-    # 「/wiki」と発言したら wiki と返す処理
-    if re.search('/wiki', message.content):
+    # 「/wiki」と発言したら wiki の要約を返す
+    if message.content.startswith('/wiki'):
         search_word = message.content.strip('/wiki ')
         print(search_word)
-        wiki_summary = WikiApi.wiki_summary(search_word)
+        wiki_summary = get_wiki_summary(search_word)
         await message.channel.send(wiki_summary)
-
 
 # Botの起動とDiscordサーバーへの接続
 client.run(WIKI_SUMMARY_TOKEN)
-
-class WikiApi:
-    wikipedia.set_lang("ja")
-    def wiki_summary(search_word):
-        try:
-            result = wikipedia.page(search_word)
-            return result.summary
-        except wikipedia.exceptions.DisambiguationError  as e:
-            return 'wikiの結果が上手く返ってきませんでした。より具体的なワードで検索してみてください。'
